80ec2356b2ac61495e8bb726d2bca4f667b124e3
leenahinduja <leenahinduja04@gmail.com>
2024-10-11 09:28:26 +0530

    Create prime.py

---
+++ prime.py
@@ -0,0 +1,15 @@
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+# Predefined number to check
+number_to_check = 29
+
+if is_prime(number_to_check):
+    print(f"{number_to_check} is a prime number.")
+else:
+    print(f"{number_to_check} is not a prime number.")
